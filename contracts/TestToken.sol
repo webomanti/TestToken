4ca21e4800b39d1bb3d8cb3161a4becfe4ce5260
webomanti <webomanti@gmail.com>
2019-04-01 19:37:30 +0200

    v8

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,12 +1,22 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract TestToken {
-	// Constructor
-	// Set the total nuber of tokens
-	// Red the total number of tokens
+	string public name = "TestToken";
+	string public symbol = "TETO";
+	string public standard = "TestToken v1.0";
 	uint256 public totalSupply;
+	mapping(address => uint256) public balanceOf;
 
 	constructor(uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
 		totalSupply = _initialSupply;
+	}
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        //balanceOf[msg.sender] -= _value;
+        //balanceOf[_to] += _value;
+        //Transfer(msg.sender, _to, _value);
+        return true;
 	}
 }
--- test/TestToken.js
+++ test/TestToken.js
@@ -1,14 +1,42 @@
 var TestToken = artifacts.require("TestToken");
 
 contract('TestToken', function(accounts) {
+	it('initialize', function() {
+		return TestToken.deployed().then(function(instance) {
+			tokenInstance = instance	
+			return tokenInstance.name();
+		}).then(function(name){
+			assert.equal(name, "TestToken", "has correct  name");
+			return tokenInstance.symbol();
+		}).then(function(symbol){
+			assert.equal(symbol, "TETO", "has correct symbol");
+			return tokenInstance.standard();
+		}).then(function(standard){
+			assert.equal(standard, "TestToken v1.0", "has correct standard");
+		});
+	});
 
 	it('sets the total supply upon deployment', function() {
 		return TestToken.deployed().then(function(instance) {
 			tokenInstance = instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply) {
-			assert.equal(totalSupply.toNumber(), 10000000, 
-						'setst the total supply to 10,000,000');
-		})
+			assert.equal(totalSupply.toNumber(), 10000000, 'sets the total supply to 10,000,000');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(adminBalance) {
+			assert.equal(adminBalance.toNumber(), 10000000, 'it allocates initial supply to admin account')
+		});
 	});
-})
+
+
+	it('transfers token ownership', function() {
+    	return TestToken.deployed().then(function(instance) {
+      		tokenInstance = instance;
+      		return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+      	}).then(assert.fail).catch(function(error) {
+     		assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+      		//return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+		});
+	});
+
+});
